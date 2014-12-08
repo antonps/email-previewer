25d77c619c2eae82fd2478b2651436e5f87c4736
Anton Saputro <antonpsaputro@gmail.com>
2014-12-08 10:19:03 +0700

    Fix responsive using outline

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 </head>
 <body style="font-family: Helvetica, sans-serif; font-size: 14px; margin: 0; line-height: 1.4">
 
-	<div style="width: 100%; max-width: 700px; margin: 0 auto; border-top: 5px solid #C40001; border-right: 1px solid #ddd; border-left: 1px solid #ddd;"> <!-- Main Wrapper -->
+	<div style="width: 100%; max-width: 700px; margin: 0 auto; outline: 1px solid #ddd;"> <!-- Main Wrapper -->
 
 			<div style="padding: 15px">
 				<h5>Brand Logo</h5>
--- template.php
+++ template.php
@@ -3,7 +3,7 @@
 <div style="padding: 15px;">
 	<h1 style="font-family: Helvetica, sans-serif; font-weight: normal">Headline</h1>
 	<p style="font-family: Helvetica, sans-serif;">Content Here</p>
-	<a href="#" style="font-family: Helvetica, sans-serif; text-decoration: none; color: white; padding-top: 8px; padding-bottom: 8px; padding-left: 15px; padding-right: 15px; font-size: 18px; background-color: #C40001">Link</a>
+	<a href="#" style="font-family: Helvetica, sans-serif; text-decoration: none; color: white; padding-top: 8px; padding-bottom: 8px; padding-left: 15px; padding-right: 15px; font-size: 18px; background-color: #15c;">Link</a>
 </div>
 
 <?php include('footer.php') ?>
